4a88ae32a0b06d1317774b73cefe924c7ad2bfe6
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2022-12-27 15:45:55 +0530

    Update Lvl1.sql

--- interviewqs/Lvl1.sql
+++ interviewqs/Lvl1.sql
@@ -5,6 +5,19 @@ day ,
 sum(score_points) over(partition by gender order by day) as total
 from scores order by 1 ;
 
+---Biggest Single Number
+
+-- finding the seconf largest number from the table
+--group by them and using windo function and ranking them and 
+select num  from(
+select num ,
+row_number() 
+    over(order by num desc) rank  from my_numbers 
+    group by num ) tmp
+    where rank = 2 
+    order by num
+    ;
+
 --- Delete Duplicate emails
 --use of sub querries and aggregat functions
 delete 
