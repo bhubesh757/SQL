227a0e61fae5bf29ebac0dcbfd8e54d172227dbe
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2022-12-27 15:36:53 +0530

    Update Lvl1.sql

--- interviewqs/Lvl1.sql
+++ interviewqs/Lvl1.sql
@@ -5,4 +5,14 @@ day ,
 sum(score_points) over(partition by gender order by day) as total
 from scores order by 1 ;
 
+--- Delete Duplicate emails
+--use of sub querries and aggregat functions
+delete 
+from person
+where id not in
+(select min(id) from person group by email  );
+
+select * from person;
+
+
 
