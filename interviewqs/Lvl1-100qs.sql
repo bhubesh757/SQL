c43817a5c3971b451f203309361ab72189e2eea1
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2022-12-28 21:51:12 +0530

    Update Lvl1-100qs.sql

--- interviewqs/Lvl1-100qs.sql
+++ interviewqs/Lvl1-100qs.sql
@@ -81,3 +81,16 @@ from logins
 where extract (yaear from time_stamp) =2020
 group by user_id ;
 
+8)Consecutive Available Seats
+
+select 
+distinct c1.seat_id as seat_id
+from 
+cinema c1  join cinema c2 
+on  abs(c1.seat_id - c2.seat_id) = 1 and
+c1.free = true and c2.free = true
+order by c1.seat_id
+;
+
+
+
